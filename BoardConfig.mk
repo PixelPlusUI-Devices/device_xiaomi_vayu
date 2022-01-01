1f53af67bebc1ad55d962ff10c87fa85dd2ced56
johnmart19 <johnivan19999@gmail.com>
2022-01-01 17:20:02 +0200

    sm8250-common: Setup LLVM kernel flags & Remove warnings - Reqired for correct build env with ThinLTO
    This commit includes changes:
    - sm8250-common: Silence clang warnings during kernel compile
    clang-12: warning: argument unused during compilation: '-fuse-ld=lld' [-Wunused-command-line-argument]
    
    - sm8250-common: Setup kernel LLVM Flags
    
    Co-authored-by: krasCGQ <kras@raphielgang.org>
    Co-authored-by: ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,6 +109,9 @@ BOARD_KERNEL_CMDLINE := \
 BOARD_MKBOOTIMG_ARGS := --header_version 2
 
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_ADDITIONAL_FLAGS := LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
+TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1
 TARGET_KERNEL_CONFIG := vayu_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/vayu
 
