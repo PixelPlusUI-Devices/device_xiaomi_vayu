lowpass777/android_device_xiaomi_haydn

vayu: Add Moto Dolby
Values taken from OP9 Pro and modified by Sasha (@helenius147 on Telegram)
to work with Moto Dolby.

- Additionally remove dax-default.xml from proprietary-files.txt
- Improve audio_effects.xml changes
- Move props from product.prop to vendor.prop
- Dolby: We have 2 Output channels for Speakers
- Disable AUDIO_FEATURE_ENABLED_DS2_DOLBY_DAP
- ro.control_privapp_permissions=log -> force
- Remove media_codecs_dolby_audio.xml, because we are not having backend for this.
- Remove libswvqe Deps, they are not existing here for MotoDolby
- Adapt for Enforcing A12.1 Sepolicy
- Add # Dolby declarations for Sepolicy

Co-Authored-By: blankaf <realblankaf@gmail.com>
Co-Authored-By: MB <mb150996@gmail.com>
Co-Authored-By: johnmart19 <johnivan19999@gmail.com>

device.mk
@@ -104,6 +104,17 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_platform_info.xml
 
+# Audio Motodolby
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.audio.dolby.ds2.enabled=false \
+    vendor.audio.dolby.ds2.hardbypass=false \
+    persist.vendor.audio_fx.current=dolby \
+    ro.vendor.dolby.dax.version=DS1_2.2.0.0_r1
+
+# Audio Motodolby Config
+PRODUCT_COPY_FILES += \
+    device/xiaomi/haydn/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \

PixelPlusUI-Devices/device_xiaomi_vayu

device.mk
@@ -86,6 +86,17 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+# Audio Motodolby
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.audio.dolby.ds2.enabled=false \
+    vendor.audio.dolby.ds2.hardbypass=false \
+    persist.vendor.audio_fx.current=dolby \
+    ro.vendor.dolby.dax.version=DS1_2.2.0.0_r1
+
+# Audio Motodolby Config
+PRODUCT_COPY_FILES += \
+    device/xiaomi/vayu/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \