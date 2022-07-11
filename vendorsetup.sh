0a140e9c96c26596696996a4182a9b21a1f7391e
Sanjeev <sanjeevstunner1@gmail.com>
2022-07-11 15:18:24 +0530

    vayu: vendorsetup: Track HALs from home
    *PPUI added hals back in home org

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -53,31 +53,3 @@ else
 echo ""
 git clone https://github.com/sanjeevstunner/vendor_pixel_launcher.git vendor/pixel/launcher
 fi
-
-# HALs 
-if [[ -d "hardware/qcom-caf/sm8150/audio" ]]; then
-echo "Looking for changes in audio HAL"
-cd hardware/qcom-caf/sm8150/audio && git pull && cd ../../../..
-echo ""
-else
-echo ""
-git clone https://github.com/PixelExperience/hardware_qcom-caf_sm8150_audio.git -b twelve hardware/qcom-caf/sm8150/audio
-fi
-
-if [[ -d "hardware/qcom-caf/sm8150/media" ]]; then
-echo "Looking for changes in media HAL"
-cd hardware/qcom-caf/sm8150/media && git pull && cd ../../../..
-echo ""
-else
-echo ""
-git clone https://github.com/PixelExperience/hardware_qcom-caf_sm8150_media.git -b twelve hardware/qcom-caf/sm8150/media
-fi
-
-if [[ -d "hardware/qcom-caf/sm8150/display" ]]; then
-echo "Looking for changes in display HAL"
-cd hardware/qcom-caf/sm8150/display && git pull && cd ../../../..
-echo ""
-else
-echo ""
-git clone https://github.com/PixelExperience/hardware_qcom-caf_sm8150_display.git -b twelve hardware/qcom-caf/sm8150/display
-fi
