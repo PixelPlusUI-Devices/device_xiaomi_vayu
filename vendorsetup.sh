15938e6e72a7d7f4453de82b96becc46f296f24b
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2022-11-10 09:10:13 +0530

    vendorsetup.sh: Track dependencies from PPUI org

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,20 +1,20 @@
 # Clone/Fetch Upstream Device Dependencies
-# Device Tree
+# Device Tree [1/6]
 echo "Looking for changes in device tree"
 cd device/xiaomi/vayu && git pull && cd ../../..
 echo ""
 
-# Vendor Tree
+# Vendor Tree  [2/6]
 if [[ -d "vendor/xiaomi/vayu" ]]; then
 echo "Looking for changes in vendor tree"
 cd vendor/xiaomi/vayu && git pull && cd ../../..
 echo ""
 else
 echo ""
-git clone https://github.com/sanjeevstunner/android_vendor_xiaomi_vayu.git -b 13 vendor/xiaomi/vayu
+git clone https://github.com/PixelPlusUI-Devices/vendor_xiaomi_vayu -b tiramisu vendor/xiaomi/vayu
 fi
 
-# Hardware Tree
+# Hardware Tree [3/6]
 if [[ -d "hardware/xiaomi" ]]; then
 echo "Removing prebuilt hardware tree"
 rm -rf hardware/xiaomi
@@ -22,27 +22,27 @@ git clone https://github.com/sanjeevstunner/android_hardware_xiaomi.git -b 13 ha
 echo ""
 fi
 
-# Kernel Tree
+# Kernel Tree [4/6]
 if [[ -d "kernel/xiaomi/vayu" ]]; then
 echo "Looking for changes in kernel tree"
 cd kernel/xiaomi/vayu && git pull && cd ../../..
 echo ""
 else
 echo ""
-git clone --depth 1 https://github.com/sanjeevstunner/android_kernel_xiaomi_vayu -b main kernel/xiaomi/vayu
+git clone --depth 1 https://github.com/PixelPlusUI-Devices/kernel_xiaomi_vayu -b tiramisu kernel/xiaomi/vayu
 fi
 
-# Mi Parts
+# Mi Parts [5/6]
 if [[ -d "vendor/xiaomi/devicesettings" ]]; then
 echo "Looking for changes in MiParts Repo"
 cd vendor/xiaomi/devicesettings && git pull && cd ../../..
 echo ""
 else
 echo ""
-git clone https://github.com/sanjeevstunner/android_vendor_xiaomi_device_settings.git -b los vendor/xiaomi/devicesettings
+git clone https://github.com/PixelPlusUI-Devices/vayu_vendor_xiaomi_device_settings -b tiramisu vendor/xiaomi/devicesettings
 fi
 
-# Google Camera
+# Google Camera [6/6]
 if [[ -d "vendor/Gcam" ]]; then
 echo "Looking for changes in GCam Repo"
 cd vendor/Gcam && git pull && cd ../..
