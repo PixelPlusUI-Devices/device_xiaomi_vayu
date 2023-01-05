3ce3bdcc3493e25e8795083ded98b7c14ccfed2c
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2023-01-05 10:43:40 +0530

    vendorsetup: Update device parts revision

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,5 +2,5 @@ git clone https://github.com/PixelPlusUI-Devices/vendor_xiaomi_vayu -b tiramisu 
 rm -rf hardware/xiaomi
 git clone https://github.com/sanjeevstunner/android_hardware_xiaomi.git -b 13 hardware/xiaomi
 git clone --depth 1 https://github.com/PixelPlusUI-Devices/kernel_xiaomi_vayu -b tiramisu kernel/xiaomi/vayu
-git clone https://github.com/PixelPlusUI-Devices/vayu_vendor_xiaomi_device_settings -b tiramisu vendor/xiaomi/devicesettings
+git clone https://github.com/PixelPlusUI-Devices/vayu_vendor_xiaomi_device_settings -b qpr1 vendor/xiaomi/devicesettings
 git clone https://gitlab.com/sanjeevstunner/vendor-g-cam-bsg.git -b main vendor/Gcam
